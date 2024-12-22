042d5e73b292bab50a8594a4b842d6e3a7405922
ShawarmaMeister <ivancrazy2000@mail.ru>
2024-12-22 16:02:20 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // 1) Создать переменную, содержащую ваше любимое время года
+        System.out.print("Введите ваше любимое время года (WINTER, SPRING, SUMMER, AUTUMN): ");
+        String input = scanner.nextLine().toUpperCase();
+
+        try {
+            Season favoriteSeason = Season.valueOf(input);
+            System.out.println("Ваше любимое время года: " + favoriteSeason);
+            System.out.println("Средняя температура: " + favoriteSeason.getAverageTemperature() + "°C");
+            System.out.println("Описание: " + favoriteSeason.getDescription());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Неверное время года!");
+        }
+
+        // 2) Метод, который принимает на вход переменную enum типа
+        printSeasonMessage(Season.SUMMER);
+
+        // 6) В цикле распечатать все времена года, среднюю температуру и описание времени года
+        for (Season season : Season.values()) {
+            System.out.println(season + " - Средняя температура: " + season.getAverageTemperature() + "°C, " + season.getDescription());
+        }
+    }
+
+    public static void printSeasonMessage(Season season) {
+        switch (season) {
+            case WINTER:
+                System.out.println("Я люблю зиму");
+                break;
+            case SPRING:
+                System.out.println("Я люблю весну");
+                break;
+            case SUMMER:
+                System.out.println("Я люблю лето");
+                break;
+            case AUTUMN:
+                System.out.println("Я люблю осень");
+                break;
         }
     }
 }
